6afeb1519cd040c772712491adc350823fdc0cd1
Simon CROS <simon.dc.cros@gmail.com>
2023-03-15 15:44:48 +0100

    Copied algo

--- src/main.c
+++ src/main.c
@@ -1,20 +1,29 @@
 #include "lem_in.h"
 
+static int	get_score(int ants, t_list *paths)
+{
+	// TODO
+	return 1;
+}
+
 void	lem_in(int ants, t_room *start, t_room *end, t_room *rooms, t_link *links, int rooms_len, int links_len)
 {
 	int		checks;
-	t_clist	best;
+	t_list	best;
 	int		best_score;
-	t_clist	results;
+	t_list	results;
 	int		results_score;
+	t_link	*cross;
 	int		i;
+	t_room	*current;
+	t_list	*path;
 
-	clst_init(&best, NULL);
+	lst_init(&best, lst_destroy);
 	best_score = -1;
 	checks = fmini3(ants, start->links.size, end->links.size);
 	while (TRUE)
 	{
-		clst_init(&results, NULL);
+		lst_init(&results, lst_destroy);
 		results_score = -1;
 
 		i = 0;
@@ -23,6 +32,76 @@ void	lem_in(int ants, t_room *start, t_room *end, t_room *rooms, t_link *links, 
 			if (links[i].mask != LINK_BOTH)
 				links[i].mask = LINK_NONE;
 			i++;
+		}
+
+		while (TRUE)
+		{
+			if (results.size >= checks)
+				break;
+
+			i = 0;
+			while (i < rooms_len)
+			{
+				rooms[i].selected = FALSE;
+				i++;
+			}
+
+			if (bfs(start, end, &cross))
+			{
+				path = lst_new(NULL);
+				if (!path)
+				{
+					// TODO free
+				}
+				current = end;
+				while (current)
+				{
+					if (!lst_unshift(&path, current))
+					{
+						// TODO free
+					}
+					if (current->parent)
+					{
+						t_iterator	it = iterator_new(&current->parent->links);
+						while (iterator_has_next(&it))
+						{
+							t_link *link = (t_link *)iterator_next(&it);
+							if (link->left == current)
+								link->mask = LINK_LEFT;
+							if (link->right == current)
+								link->mask = LINK_RIGHT;
+						}
+					}
+					current = end->parent;
+				}
+				if (!lst_unshift(&results, path))
+				{
+					// TODO free
+				}
+				int	score = get_score(ants, &results);
+				if (score < results_score || results_score == -1)
+				{
+					results_score = score;
+				}
+				else
+					break;
+			}
+			else
+			{
+				if (cross)
+					cross->mask = LINK_BOTH;
+				cross = NULL;
+				break;
+			}
+
+			if (results_score != -1 && (results_score < best_score || best_score == -1))
+			{
+				lst_clear(&best);
+				best = results;
+				best_score = results_score;
+			}
+			else
+				break;
 		}
 	}
 }
--- src/utils.c
+++ src/utils.c
@@ -1,31 +1,11 @@
 #include "lem_in.h"
 
-void	*room_to_best_entry(t_room *room)
+void	*room_to_bfs_open_entry(t_room *room)
 {
 	return ((void *)room + 0);
 }
 
-void	*room_to_results_entry(t_room *room)
-{
-	return ((void *)room + 8);
-}
-
-void	*room_to_bfs_open_entry(t_room *room)
-{
-	return ((void *)room + 16);
-}
-
-t_room	*best_entry_to_room(void *entry)
-{
-	return ((t_room *)entry - 0);
-}
-
-t_room	*results_entry_to_room(void *entry)
-{
-	return ((t_room *)entry - 8);
-}
-
 t_room	*bfs_open_entry_to_room(void *entry)
 {
-	return ((t_room *)entry - 16);
+	return ((t_room *)entry - 0);
 }
