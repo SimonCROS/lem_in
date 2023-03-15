93f8bdb13e5321c00ec90820d34ce463f29c7d65
Simon CROS <simon.dc.cros@gmail.com>
2023-03-15 17:17:41 +0100

    Secure with clear

--- src/main.c
+++ src/main.c
@@ -18,12 +18,54 @@ static int	get_score(int ants, t_list *paths)
 	return (1);
 }
 
-static void	get_next_path_group(t_lem_in *data, t_list *results, int *results_score)
+static void	disable_link_to(t_room *room)
 {
-	t_link	*cross;
-	t_room	*current;
-	t_list	*path;
-	int		i;
+	t_iterator	it;
+	t_link		*link;
+
+	if (room->parent)
+	{
+		it = iterator_new(&room->parent->links);
+		while (iterator_has_next(&it))
+		{
+			link = (t_link *)iterator_next(&it);
+			if (link->left == room)
+				link->mask = LINK_LEFT;
+			if (link->right == room)
+				link->mask = LINK_RIGHT;
+		}
+	}
+}
+
+static t_list	*build_path_from_parents(t_room *last)
+{
+	t_list		*path;
+	t_room		*current;
+
+	path = lst_new(NULL);
+	if (!path)
+		return NULL;
+	current = last;
+	while (current)
+	{
+		if (!lst_unshift(path, current))
+		{
+			lst_destroy(path);
+			return NULL;
+		}
+		disable_link_to(current);
+		current = current->parent;
+	}
+
+	return path;
+}
+
+static char	get_path_group(t_lem_in *data, t_list *results, int *results_score)
+{
+	t_link		*cross;
+	t_list		*path;
+	int			i;
+	int			score;
 
 	lst_init(results, (t_consumer)lst_destroy);
 	*results_score = -1;
@@ -39,37 +81,13 @@ static void	get_next_path_group(t_lem_in *data, t_list *results, int *results_sc
 		}
 		if (bfs(data->start, data->end, &cross))
 		{
-			path = lst_new(NULL);
-			if (!path)
+			path = build_path_from_parents(data->end);
+			if (!path || !lst_unshift(results, path))
 			{
-				// TODO free
+				lst_clear(results);
+				return (FALSE);
 			}
-			current = data->end;
-			while (current)
-			{
-				if (!lst_unshift(path, current))
-				{
-					// TODO free
-				}
-				if (current->parent)
-				{
-					t_iterator	it = iterator_new(&current->parent->links);
-					while (iterator_has_next(&it))
-					{
-						t_link	*link = (t_link *)iterator_next(&it);
-						if (link->left == current)
-							link->mask = LINK_LEFT;
-						if (link->right == current)
-							link->mask = LINK_RIGHT;
-					}
-				}
-				current = data->end->parent;
-			}
-			if (!lst_unshift(results, path))
-			{
-				// TODO free
-			}
-			int	score = get_score(data->ants, results);
+			score = get_score(data->ants, results);
 			if (score < *results_score || *results_score == -1)
 			{
 				*results_score = score;
@@ -85,6 +103,8 @@ static void	get_next_path_group(t_lem_in *data, t_list *results, int *results_sc
 			break ;
 		}
 	}
+
+	return (TRUE);
 }
 
 void	lem_in(t_lem_in *data)
@@ -95,7 +115,8 @@ void	lem_in(t_lem_in *data)
 
 	lst_init(&data->best, (t_consumer)lst_destroy);
 	data->best_score = -1;
-	data->checks = fmini3(data->ants, data->start->links.size, data->end->links.size);
+	data->checks = fmini3(data->ants,
+			data->start->links.size, data->end->links.size);
 	while (TRUE)
 	{
 		i = 0;
@@ -105,11 +126,21 @@ void	lem_in(t_lem_in *data)
 				data->links[i].mask = LINK_NONE;
 			i++;
 		}
-		get_next_path_group(data, &results, &results_score);
-		if (results_score != -1 && (results_score < data->best_score || data->best_score == -1))
+		if (!get_path_group(data, &results, &results_score))
 		{
 			lst_clear(&data->best);
-			data->best = results;
+			lst_clear(&results);
+			return ;
+		}
+		if (results_score != -1
+			&& (results_score < data->best_score || data->best_score == -1))
+		{
+			lst_clear(&data->best);
+			if (!lst_copy_to(&results, &data->best))
+			{
+				lst_clear(&results);
+				return ;
+			}
 			data->best_score = results_score;
 		}
 		else
