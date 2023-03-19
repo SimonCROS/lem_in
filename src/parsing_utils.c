f24e691c207bec83ef2d87f8577509e2f00f3733
SimonCROS <scros@student.42lyon.fr>
2023-03-19 14:16:08 +0100

    Fix norme in read_all_lines

--- src/parsing_utils.c
+++ src/parsing_utils.c
@@ -12,38 +12,45 @@
 
 #include "lem_in.h"
 
+static char	**free_ret_null(char *s1, char *s2)
+{
+	if (s1)
+		free(s1);
+	if (s2)
+		free(s2);
+	return (NULL);
+}
+
+static char	*join_free(char *s1, char *s2)
+{
+	char	*ret;
+
+	ret = ft_strjoin(s1, s2);
+	free(s1);
+	free(s2);
+	return (ret);
+}
+
 char	**read_all_lines(int fd)
 {
 	char	**result;
 	char	*content;
-	char	*tmp;
 	char	*buf;
 	ssize_t	rd;
 
 	content = NULL;
 	buf = NULL;
-	tmp = NULL;
 	rd = 1;
 	while (rd > 0)
 	{
 		buf = malloc(BUFFER_SIZE + 1);
 		if (!buf)
-		{
-			free(content);
-			return (NULL);
-		}
+			return (free_ret_null(content, NULL));
 		rd = read(fd, buf, BUFFER_SIZE);
 		if (rd == -1)
-		{
-			free(content);
-			free(buf);
-			return (NULL);
-		}
+			return (free_ret_null(content, buf));
 		buf[rd] = '\0';
-		tmp = content;
-		content = ft_strjoin(tmp, buf);
-		free(tmp);
-		free(buf);
+		content = join_free(content, buf);
 		if (!content)
 			return (NULL);
 	}
